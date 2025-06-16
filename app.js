f070bb93160fdb890f8b25d8b36d562eaabdce8f
Joao Inacio <joaoinacio322@gmail.com>
2025-06-16 20:16:35 -0300

    Adiciona função para reiniciar

--- app.js
+++ app.js
@@ -40,4 +40,11 @@ function alterarStatusBotao(){
   }
 }
 
+function reiniciar(){
+  document.getElementById('quantidade').value = '';
+  document.getElementById('de').value = '';
+  document.getElementById('ate').value = '';
+  document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+  alterarStatusBotao();
+}
 
