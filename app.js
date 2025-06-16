aae46fa4363a45f428906fef815b6d753d0af2ca
Joao Inacio <joaoinacio322@gmail.com>
2025-06-16 20:04:02 -0300

    Alterando o status do botao

--- app.js
+++ app.js
@@ -18,12 +18,26 @@ function sortear(){
 
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+    alterarStatusBotao();
+
 
     
 }
 
 function gerarNumeroAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao(){
+  let botao = document.getElementById('btn-reiniciar');
+  if (botao.classList.contains('container__botao-desabilitado')){
+    botao.classList.remove('container__botao-desabilitado');
+    botao.classList.add('container__botao');
+  }else{
+    botao.classList.add('container__botao-desabilitado');
+    botao.classList.remove('container__botao');
+
+  }
 }
 
 
